004413f254085f7d374fc91b76b454ec6fe1923c
Damon Allison <damon@shipt.com>
2020-05-13 08:20:33 -0500

    Getting this versioning thing down

--- v2/calculator/calculator.go
+++ v2/calculator/calculator.go
@@ -1,13 +1,14 @@
-// Package v2 is a simple test calculator created to test go modules
+// Package calculator is a simple test calculator created to test go modules
 //
 // Notice that this is semantic version "v2", which breaks backward compatibility
 // with v1. Thus, a new version.
-package v2
+package calculator
 
 // AddInts adds 2 ints
 func AddInts(x, y int) int {
 	return x + y
 }
+
 // SubtractInts subtracts 2 ints
 func SubtractInts(x, y int) int {
 	return x - y
